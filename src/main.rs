b4150c9dbbfa6b1d35bef5b6c64a221be1fbf5b8
lalala_233 <lalala_233@qq.com>
2025-11-03 09:22:12 +0800

    feat: return exit code when an error occurs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::process::exit;
 use webdav_backup::run;
 
 #[tokio::main]
 async fn main() {
     if let Err(error) = run().await {
         eprintln!("{error}");
+        exit(1);
     }
 }
